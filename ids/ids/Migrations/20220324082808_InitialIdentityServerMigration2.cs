fcf01ce72c62ae15b74bce4bc6c63bce09b8262a
nurmakhan_pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-03-28 16:16:23 +0600

    first commit

---
+++ ids/ids/Migrations/20220324082808_InitialIdentityServerMigration2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ids.Migrations
+{
+    public partial class InitialIdentityServerMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Data",
+                table: "PersistedGrants",
+                type: "nvarchar(max)",
+                maxLength: 50000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50000)",
+                oldMaxLength: 50000);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Expiration",
+                table: "DeviceCodes",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Data",
+                table: "DeviceCodes",
+                type: "nvarchar(max)",
+                maxLength: 50000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50000)",
+                oldMaxLength: 50000);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Data",
+                table: "PersistedGrants",
+                type: "nvarchar(50000)",
+                maxLength: 50000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 50000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Expiration",
+                table: "DeviceCodes",
+                type: "nvarchar(200)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Data",
+                table: "DeviceCodes",
+                type: "nvarchar(50000)",
+                maxLength: 50000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 50000);
+        }
+    }
+}
